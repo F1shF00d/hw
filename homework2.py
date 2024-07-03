0c085171ec6f7236d402db23151655f3c5a03058
F1shF00d <slavamakssimov2002@gmail.com>
2024-07-03 21:16:09 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+First_Peremennaya=12
+second_peremennaya=1.5
+name_kurs="Python"
+first_time=(second_peremennaya/First_Peremennaya)
+print('Курс:', name_kurs, ', всего задач:', First_Peremennaya, ', затрачено часов:', second_peremennaya, ',среднее время выполнения ', first_time, ' часа')
