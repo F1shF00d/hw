3839d734c69d18c74ff30a39dd0cda54deb09a52
F1shF00d <slavamakssimov2002@gmail.com>
2024-07-15 23:08:54 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_tuple = 1, True, 'world'
+print(immutable_tuple)
+#immutable_tuple[1]=False #кортеж не изменяется через индексацию
+print(immutable_tuple)
+mutable_list = 1, 'string', False
+mutable_list = 2, 'short', True
+print(mutable_list)
