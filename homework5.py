e80da717132885f8b72e433a9dd7f2696f97c0b9
F1shF00d <slavamakssimov2002@gmail.com>
2024-07-16 19:06:52 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+Immutable_tuple= (1, 2, 'a', 'b')
+print(Immutable_tuple)
+#immutable_tuple[1]=False #кортеж не изменяется через индексацию
+print(Immutable_tuple)
+Mutable_list= [1, 2, 'a', 'b', 'Modified']
+print(Mutable_list)
+Mutable_list[0]= 'slava'
+print(Mutable_list)
